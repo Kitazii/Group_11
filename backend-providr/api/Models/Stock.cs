b3e3387e2aa7ea2dbf28fb223fa0c4f1b0cec49a
rated264 <rikesh.gurung.10i.esac@gmail.com>
2024-11-01 00:47:55 +0000

    Added database

--- backend-providr/api/Models/Stock.cs
+++ backend-providr/api/Models/Stock.cs
@@ -17,7 +17,7 @@ namespace api.Models
         public decimal Purchase { get; set; }
         [Column(TypeName = "decimal(18,2)")]
 
-        public decimal LastDiv { get; set; }
+        public decimal LastDiv {get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
 
--- backend-providr/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend-providr/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8772ec8c5b29451a560fcb1fb49163c8ed30937")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
