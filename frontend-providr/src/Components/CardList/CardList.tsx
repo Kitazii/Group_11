c37259e53091882e56564ba6b84c480db7f284a2
Erin Forbes <163554441+eforbe301@users.noreply.github.com>
2024-11-29 12:36:32 +0000

    lists, install uuid

--- frontend-providr/src/Components/CardList/CardList.tsx
+++ frontend-providr/src/Components/CardList/CardList.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Card from '../Card/Card';
 import "./CardList.css";
 
-interface Props {}
-
-const CardList : React.FC<Props> = (props: Props): JSX.Element => {
-  return <div>:
-    <Card businessName='Wallace Plumbing' businessCity="Glasgow"/>
-    <Card businessName='Conway Scaffolding' businessCity="Saltcoats"/>
-    <Card businessName='Skip Hire 101' businessCity="Edinburgh"/>
-    </div>
+interface Props {
+  searchResults: CompanySearch[];
+}
+const CardList : React.FC<Props> = ({ searchResults }:Props): JSX.Element => {
+  return <>
+  {searchResults.length > 0 ? (
+    searchResults.map((result) => {
+      return <Card id={result.symbol} key={} />;
+    })
+  ): (
+    <h1>No results</h1>
+  )}</>;
+  
 };
 
 export default CardList;
